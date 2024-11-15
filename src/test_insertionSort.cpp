91acc32781807d93bb44794a489be2cf8178825b
“tls0523” <litsin0523@gmail.com>
2024-11-15 18:52:48 +0800

    added test cpp files

---
+++ src/test_insertionSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class InsertionSort {
+public:
+    void sort(int* arr, int size) {
+        for (int i = 1; i < size; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = key;
+        }
+    }
+};
+
+int main() {
+    int arr[6];
+    int size = 6;
+
+    arr[0] = 12;
+    arr[1] = 11;
+    arr[2] = 13;
+    arr[3] = 5;
+    arr[4] = 6;
+    arr[5] = 7;
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    InsertionSort sorter;
+    sorter.sort(arr, size);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
